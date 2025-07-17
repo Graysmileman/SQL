5190545961cf90d905a318fc961642d23e339ce4
Jitpanu <71390486+Graysmileman@users.noreply.github.com>
2025-07-17 15:42:24 +0700

    Add files via upload

---
+++ 68.sql
@@ -0,0 +1,5 @@
+select * from Employees
+select EmployeeID, FirstName, LastName from Employees
+select * from Employees where City = 'London'
+select City, country from Customers
+select distinct city, country from customers
