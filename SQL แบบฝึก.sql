6f9465b7ce967f35f9ff2cd6bec19194f1625410
Jitpanu <71390486+Graysmileman@users.noreply.github.com>
2025-08-07 14:09:54 +0700

    Update SQL แบบฝึก 7/8/68 .sql

--- SQL แบบฝึก.sql
+++ SQL แบบฝึก.sql
@@ -10,8 +10,8 @@ select *
 from products where UnitsInStock < 250
 
 -- 3.รหัสสินค้า ชื่อสินค้าที่เลิกจำหน่ายเเล้ว
-select *
-from Products where Discontinued = 0
+select productID, productname,	
+from Products where Discontinued = 1
 
 -- 4. รหัสสินค้า ชื่อสินค้า ราคา ของสินค้าที่มีราคามากกว่า 100 
 select ProductID, ProductName, UnitPrice 
@@ -22,12 +22,10 @@ select productid, unitprice
 from Products where ProductName='ยางลบ'
 
 -- 6. หมายเลขใบเสร็จ วันที่ และ ราคา ของใบเสร็จที่ออกก่อนที่วันที่ 15 ก.พ.
-
-
-
+select ReceiptID, ReceiptDate,TotalCash from Receipts where ReceiptDate < '2013-02-15'
 
 -- 7.รหัสสินค้า ชื่อสินค้า ที่มีจำนวนคงเหลือตั้งเเต่ 400 ขึ้นไป
-select productid, productname
+select productid, productname,UnitsInStock
 from Products where UnitsInStock >400
 
 -- 8.รหัสสินค้า ชื่อสินค้า ราคา และจำนวนคงเหลือ ของเเชม,แป้งเด็ก,ดินสอ,ยางลบ
@@ -36,7 +34,19 @@ from Products where productname In ('แชมพู', 'แป้งเด็ก
 
 -- 9. รายละเอียดของสินค้าประเภทเครื่องเขียน
 select  Description 
-from Categories where CategoryID = 1
+from Categories where Categories = 'เครื่องเขียน'
 
 -- 10. รหัสประเภทสินค้า ชื่อประเภท และรายละเอียดของ สินค้าประเภทเครื่องสำอาง
-select
+select CategoryID, CategoryName,[Description] from Categories where CategoryName='เครื่องสำอาง'
+-- 11.คำนำหน้า ชื่อ นามสกุล ของพนักงานที่เป็น Sale Representative
+select Title, FirstName, LastName from Employees where Position='Sale representative'
+-- 12. รหัสพนักงาน ชื่อพนักงาน ชื่อผู้ใช้ รหัสผ่าน ของพนักงานทุกคน
+select Title+FirstName+space(1)+LastName EmpName, UserName, Password from Employees
+
+-- 13. ชื่อผู้ใช้ และรหัสผ่านของพนักงานที่ชื่อก้องนิรันดร์
+select UserName, Password from Employees where FirstName='ก้องนิรันดร์'
+
+-- 14. รหัสพนักงานที่ออกใบเสร็จหมายเลข 3
+select EmployeeID from Receipts where ReceiptID = 3
+-- 15. รหัสสินค้า ชื่อสินค้า ราคา ของสินค้าที่มีรหัสประเภท 2, 4
+select productID,ProductName,UnitPrice from Products where CategoryID in (2,4)
