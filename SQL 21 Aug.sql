68443a554764dfb358fc97b5320b4891bf00dc07
Jitpanu <71390486+Graysmileman@users.noreply.github.com>
2025-08-21 13:53:33 +0700

    Update SQL 21 Aug.sql
    อิอิ

--- SQL 21 Aug.sql
+++ SQL 21 Aug.sql
@@ -1,1 +1,30 @@
-สวัสดีครับ
+-- 1.
+Select CategoryName, PRODUCTName, Unitprice
+from products as p, categories as c where p.CategoryID=c.CategoryID
+-- 2. 
+Select CategoryName, PRODUCTName, Unitprice
+from products as p join categories as c on p.CategoryID=c.CategoryID
+-- 3. ต้องการแสดงเฉพาะ ประเภทสินค้า
+Select CategoryName, PRODUCTName, Unitprice
+from products as p, categories as c 
+where p.CategoryID=c.CategoryID
+and CategoryName = 'seafood'
+-- 3.(2)
+Select CategoryName, PRODUCTName, Unitprice
+from products as p join categories as c 
+on p.CategoryID=c.CategoryID
+where CategoryName ='seafood'
+-- 4. (Cartesian Product)
+select companyname, orderid
+from Orders, shippers 
+where shippers.shipperid = orders.shipvia
+--(Join Operator)
+select companyname, orderid
+from Orders join shippers 
+on shippers.shipperid = orders.shipvia
+
+select companyname, orderid
+from Orders, shippers 
+where shippers.shipperid = orders.shipvia
+and 
+
